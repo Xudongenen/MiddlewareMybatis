e713b501b624922e6bc60540e8a547d455b6fddb
xudongenen <1535941400@qq.com>
2022-04-19 11:48:12 +0800

    Initial commit

---
+++ src/main/java/com/zxd/middlewaremybatis/MiddlewareMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.zxd.middlewaremybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddlewareMybatisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zxd/middlewaremybatis/MiddlewareMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxd.middlewaremybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddlewareMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
